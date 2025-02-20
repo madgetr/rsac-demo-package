028288b63d110859d11908cc408586f98ca0002c
Trevor Madge <tmadge@sonatype.com>
2025-02-20 12:52:41 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='rsac_2025',
+    version='0.1',
+    description='For Research Purposes Only',
+    author='Trevor Madge',
+    author_email='tmadge@sonatype.com',
+    packages=['rsac_2025'],
+)
